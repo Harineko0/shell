ac3dff8aecdbc22ba99274e206656556ca270aaf
harineko0 <harineko0927@gmail.com>
2024-07-05 18:09:18 +0900

    FIX: history コマンド修正

--- src/cmd/history.c
+++ src/cmd/history.c
@@ -5,18 +5,13 @@
 int history(char **argv) {
     int count = 0;
     int size = cmd_stack->size;
-    int bottom = cmd_stack->bottom;
-    char *item = cmd_stack->items[bottom];
+    int top = cmd_stack->top;
+    int bottom = dir_stack->bottom;
 
-    while (item != NULL && count < size) {
-        printf(" %d %s\n", count++, item);
-
-        bottom++;
-        if (bottom >= size) {
-            bottom = 0;
-        }
-
-        item = cmd_stack->items[bottom];
+    for (int i = bottom; i != top; i = (i + 1) % size) {
+        char *item = cmd_stack->items[i];
+        if (item != NULL)
+            printf(" %d %s\n", count++, item);
     }
 
     return 0;
